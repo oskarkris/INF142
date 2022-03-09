5c91cafb023f7685e1550203da693d39eda3eeda
oskar <oskar_kristoffersen@hotmail.com>
2022-03-09 12:10:20 +0100

    mangler bare database

--- client.py
+++ client.py
@@ -6,76 +6,16 @@ from rich.table import Table
 from champlistloader import load_some_champs
 from core import Champion, Match, Shape, Team
 from socket import socket
+import pickle
 
-champions = load_some_champs()
-
-def print_available_champs(champions: dict[Champion]) -> None:
-    global available_champs
-    # Create a table containing available champions
-    available_champs = Table(title='Available champions')
-
-    # Add the columns Name, probability of rock, probability of paper and
-    # probability of scissors
-    available_champs.add_column("Name", style="cyan", no_wrap=True)
-    available_champs.add_column("prob(:raised_fist-emoji:)", justify="center")
-    available_champs.add_column("prob(:raised_hand-emoji:)", justify="center")
-    available_champs.add_column("prob(:victory_hand-emoji:)", justify="center")
-
-    # Populate the table
-    for champion in champions.values():
-        available_champs.add_row(*champion.str_tuple)
-
-def print_match_summary(match: Match) -> None:
-
-    EMOJI = {
-        Shape.ROCK: ':raised_fist-emoji:',
-        Shape.PAPER: ':raised_hand-emoji:',
-        Shape.SCISSORS: ':victory_hand-emoji:'
-    }
-
-    # For each round print a table with the results
-    for index, round in enumerate(match.rounds):
-
-        # Create a table containing the results of the round
-        round_summary = Table(title=f'Round {index+1}')
-
-        # Add columns for each team
-        round_summary.add_column("Red",
-                                 style="red",
-                                 no_wrap=True)
-        round_summary.add_column("Blue",
-                                 style="blue",
-                                 no_wrap=True)
-
-        # Populate the table
-        for key in round:
-            red, blue = key.split(', ')
-            round_summary.add_row(f'{red} {EMOJI[round[key].red]}',
-                                  f'{blue} {EMOJI[round[key].blue]}')
-        print(round_summary)
-        print('\n')
-
-    # Print the score
-    red_score, blue_score = match.score
-    print(f'Red: {red_score}\n'
-          f'Blue: {blue_score}')
-
-    # Print the winner
-    if red_score > blue_score:
-        print('\n[red]Red victory! :grin:')
-    elif red_score < blue_score:
-        print('\n[blue]Blue victory! :grin:')
-    else:
-        print('\nDraw :expressionless:')
-
-print_available_champs(champions)
 
 
 sock = socket()
 sock.connect(("localhost",5555))
 while True: #figure out how to only run this while connected to server
-    print(available_champs) #figure out how to remove rows from rich.table
-    champion_select = sock.recv(1024).decode() 
+    #print(available_champs) #figure out how to remove rows from rich.table
+    champion_select = pickle.loads(sock.recv(4096)) 
+    print(champion_select)
     print(f"From Server: {champion_select}")
     champ_pick = input()
     sock.send(champ_pick.encode())
--- server.py
+++ server.py
@@ -5,7 +5,26 @@ from rich.table import Table
 from champlistloader import load_some_champs
 from core import Champion, Match, Shape, Team
 from socket import socket
+import pickle
+champions = load_some_champs()
+def print_available_champs(champions: dict[Champion]) -> None:
+    global available_champs
+    # Create a table containing available champions
+    available_champs = Table(title='Available champions')
 
+    # Add the columns Name, probability of rock, probability of paper and
+    # probability of scissors
+    available_champs.add_column("Name", style="cyan", no_wrap=True)
+    available_champs.add_column("prob(:raised_fist-emoji:)", justify="center")
+    available_champs.add_column("prob(:raised_hand-emoji:)", justify="center")
+    available_champs.add_column("prob(:victory_hand-emoji:)", justify="center")
+
+    # Populate the table
+    for champion in champions.values():
+        available_champs.add_row(*champion.str_tuple)
+
+
+print_available_champs(champions)
 def input_champion(prompt: str,
                    color: str,
                    champions: dict[Champion],
@@ -29,10 +48,52 @@ def input_champion(prompt: str,
                 conn.send(f'[{color}]{name} has been chosen by {prompt}. '.encode())
                 break
 
-champions = load_some_champs()
+
 player1 = []
 player2 = []
 
+def print_match_summary(match: Match) -> None:
+
+    EMOJI = {
+        Shape.ROCK: ':raised_fist-emoji:',
+        Shape.PAPER: ':raised_hand-emoji:',
+        Shape.SCISSORS: ':victory_hand-emoji:'
+    }
+
+    # For each round print a table with the results
+    for index, round in enumerate(match.rounds):
+
+        # Create a table containing the results of the round
+        round_summary = Table(title=f'Round {index+1}')
+
+        # Add columns for each team
+        round_summary.add_column("Red",
+                                 style="red",
+                                 no_wrap=True)
+        round_summary.add_column("Blue",
+                                 style="blue",
+                                 no_wrap=True)
+
+        # Populate the table
+        for key in round:
+            red, blue = key.split(', ')
+            round_summary.add_row(f'{red} {EMOJI[round[key].red]}',
+                                  f'{blue} {EMOJI[round[key].blue]}')
+        print(round_summary)
+        print('\n')
+
+    # Print the score
+    red_score, blue_score = match.score
+    print(f'Red: {red_score}\n'
+          f'Blue: {blue_score}')
+
+    # Print the winner
+    if red_score > blue_score:
+        print('\n[red]Red victory! :grin:')
+    elif red_score < blue_score:
+        print('\n[blue]Blue victory! :grin:')
+    else:
+        print('\nDraw :expressionless:')
 
 sock = socket()
 
@@ -41,13 +102,17 @@ sock.bind(("localhost", 5555))
 sock.listen()
 print("The server is ready to recieve")
 
+print_available_champs(champions)
+print(available_champs)
 while True:
     #figure out how to send tables to client
     conn, _=sock.accept()
+    conn.send(pickle.dumps(available_champs))
 
-    for x in range(2):
-        input_champion('Player 1', 'red', champions, player1, player2)
-        input_champion('Player 2', 'blue', champions, player2, player1)
+
+    #for x in range(2):
+        #input_champion('Player 1', 'red', champions, player1, player2)
+        #input_champion('Player 2', 'blue', champions, player2, player1)
     conn.send("CLOSING".encode())
     conn.close()
 
